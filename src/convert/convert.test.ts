4ae5148315d8077a8738f2c52ca9f5387db157c3
mytee306 <mateja176@gmail.com>
2019-02-05 21:34:04 +0100

    Adds convert.test.ts

---
+++ src/convert/convert.test.ts
@@ -0,0 +1,14 @@
+import convert from "./"
+
+describe("Convert text to JSON", () => {
+  test("It should return the value", () => {
+    expect(convert("a")).toBe("a")
+  })
+  test("It should return an object", () => {
+    expect(
+      convert(`
+a
+  b`),
+    ).toBe({ a: "b" })
+  })
+})
