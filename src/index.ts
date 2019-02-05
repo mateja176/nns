b8b4f0a1446dfe37111f2209c436750382e3767d
mytee306 <mateja176@gmail.com>
2019-02-05 18:13:15 +0100

    Adds index.ts

---
+++ src/index.ts
@@ -0,0 +1,60 @@
+import { dec, inc, slice } from "ramda";
+import convert from "./utils/convert";
+import getIndentationFor from "./utils/getIndentationFor";
+import setValueAndSelection from "./utils/setValueAndSelection";
+import splitByNewLine from "./utils/splitByNewLine";
+
+const form = document.querySelector("form") as HTMLFormElement
+
+const indentation = document.querySelector(
+  ".indentation",
+) as HTMLTextAreaElement
+
+indentation.onkeydown = e => {
+  const { key, target } = e as any
+
+  const { value, selectionStart } = target
+
+  const precedingText = slice(0, selectionStart, value)
+
+  const succeedingText = slice(selectionStart, Infinity, value)
+
+  const setValue = setValueAndSelection(e)(target)
+
+  switch (key) {
+    case "Tab":
+      setValue(`${precedingText}\t${succeedingText}`)(inc(selectionStart))
+      break
+    case "Enter":
+      const indentation = getIndentationFor(
+        dec(splitByNewLine(succeedingText).length),
+      )(splitByNewLine(value))
+
+      setValue(`${precedingText}\n${indentation}${succeedingText}`)(
+        selectionStart + 1 + indentation.length,
+      )
+
+      break
+    default:
+      break
+  }
+}
+
+form.onsubmit = e => {
+  e.preventDefault()
+
+  const {
+    target: {
+      json,
+      indentation: { value },
+    },
+  } = e as any
+
+  json.value = convert(value)
+}
+
+indentation.value = `Hello
+  World!
+How
+  are
+  you?`
